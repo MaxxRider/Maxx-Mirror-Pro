5bcf7b75f431e876cd780778bf2a7c8c0095a165
RJ HANIF AHMED <66148227+MaxxRider@users.noreply.github.com>
2020-10-31 21:24:46 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -106,7 +106,7 @@ def get_readable_message():
                 except:
                     pass
             if download.status() == MirrorStatus.STATUS_DOWNLOADING:
-                msg += f"\n○<b>CANCEL</b> <code>/cancel4 {download.gid()}</code>"
+                msg += f"\n○<b>CANCEL</b> <code>/cancel {download.gid()}</code>"
             msg += "\n\n"
         return msg
 
