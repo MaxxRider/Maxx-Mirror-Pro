ff314869c7147d2413918d09aafffc72725547f4
ADIB AKHTAB <66148227+MaxxRider@users.noreply.github.com>
2021-01-06 14:53:05 +0530

    Update mirror_status.py

--- bot/modules/mirror_status.py
+++ bot/modules/mirror_status.py
@@ -7,7 +7,7 @@ from telegram.error import BadRequest
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
 import threading
-
+ 
 @run_async
 def mirror_status(update,context):
     message = get_readable_message()
@@ -23,8 +23,8 @@ def mirror_status(update,context):
             del status_reply_dict[index]
     sendStatusMessage(update,context.bot)
     deleteMessage(context.bot,update.message)
-
-
+ 
+ 
 mirror_status_handler = CommandHandler(BotCommands.StatusCommand, mirror_status,
                                        filters=CustomFilters.authorized_chat | CustomFilters.authorized_user)
 dispatcher.add_handler(mirror_status_handler)
