8c8bc27518147c0b9be6b21f4f6fc68b369751cb
ADIB AKHTAB <66148227+MaxxRider@users.noreply.github.com>
2021-01-06 14:44:03 +0530

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -1,7 +1,7 @@
 export MAX_DOWNLOAD_SPEED=0
-export MAX_CONCURRENT_DOWNLOADS=3
+export MAX_CONCURRENT_DOWNLOADS=10
 aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 \
   --max-connection-per-server=10 --rpc-max-request-size=1024M \
-  --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 \
+  --seed-time=0.01 --bt-stop-timeout=300 --min-split-size=10M --follow-torrent=mem --split=10 \
    --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
    --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS
