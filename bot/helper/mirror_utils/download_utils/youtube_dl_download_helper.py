64740dbeabac9b4d82e8d8900c799b87f940e20e
ADIB AKHTAB <66148227+MaxxRider@users.noreply.github.com>
2021-01-06 14:59:21 +0530

    Update youtube_dl_download_helper.py

--- bot/helper/mirror_utils/download_utils/youtube_dl_download_helper.py
+++ bot/helper/mirror_utils/download_utils/youtube_dl_download_helper.py
@@ -6,30 +6,30 @@ from ..status_utils.youtube_dl_download_status import YoutubeDLDownloadStatus
 import logging
 import re
 import threading
-
+ 
 LOGGER = logging.getLogger(__name__)
-
-
+ 
+ 
 class MyLogger:
     def __init__(self, obj):
         self.obj = obj
-
+ 
     def debug(self, msg):
         LOGGER.debug(msg)
         # Hack to fix changing changing extension
         match = re.search(r'.ffmpeg..Merging formats into..(.*?).$', msg)
         if match and not self.obj.is_playlist:
             self.obj.name = match.group(1)
-
+ 
     @staticmethod
     def warning(msg):
         LOGGER.warning(msg)
-
+ 
     @staticmethod
     def error(msg):
         LOGGER.error(msg)
-
-
+ 
+ 
 class YoutubeDLHelper(DownloadHelper):
     def __init__(self, listener):
         super().__init__()
@@ -40,7 +40,8 @@ class YoutubeDLHelper(DownloadHelper):
         self.opts = {
             'progress_hooks': [self.__onDownloadProgress],
             'logger': MyLogger(self),
-            'usenetrc': True
+            'usenetrc': True,
+            'format': "best/bestvideo+bestaudio"
         }
         self.__download_speed = 0
         self.download_speed_readable = ''
@@ -51,17 +52,17 @@ class YoutubeDLHelper(DownloadHelper):
         self.is_cancelled = False
         self.vid_id = ''
         self.__resource_lock = threading.RLock()
-
+ 
     @property
     def download_speed(self):
         with self.__resource_lock:
             return self.__download_speed
-
+ 
     @property
     def gid(self):
         with self.__resource_lock:
             return self.__gid
-
+ 
     def __onDownloadProgress(self, d):
         if self.is_cancelled:
             raise ValueError("Cancelling Download..")
@@ -83,28 +84,25 @@ class YoutubeDLHelper(DownloadHelper):
                 else:
                     self.download_speed_readable = d['_speed_str']
                     self.downloaded_bytes = d['downloaded_bytes']
-
+ 
     def __onDownloadStart(self):
         with download_dict_lock:
             download_dict[self.__listener.uid] = YoutubeDLDownloadStatus(self, self.__listener)
-
+ 
     def __onDownloadComplete(self):
         self.__listener.onDownloadComplete()
-
+ 
     def onDownloadError(self, error):
         self.__listener.onDownloadError(error)
-
-    def extractMetaData(self, link, qual):
-        if 'hotstar' in link:
+ 
+    def extractMetaData(self, link):
+        if 'hotstar' or 'sonyliv' in link:
             self.opts['geo_bypass_country'] = 'IN'
-
+ 
         with YoutubeDL(self.opts) as ydl:
             try:
                 result = ydl.extract_info(link, download=False)
                 name = ydl.prepare_filename(result)
-                # noobway hack for changing extension after converting to mp3
-                if qual == "audio":
-                  name = name.replace(".mp4", ".mp3").replace(".webm", ".mp3")
             except DownloadError as e:
                 self.onDownloadError(str(e))
                 return
@@ -126,7 +124,7 @@ class YoutubeDLHelper(DownloadHelper):
             self.name = name
             self.vid_id = video.get('id')
         return video
-
+ 
     def __download(self, link):
         try:
             with YoutubeDL(self.opts) as ydl:
@@ -139,22 +137,17 @@ class YoutubeDLHelper(DownloadHelper):
         except ValueError:
             LOGGER.info("Download Cancelled by User!")
             self.onDownloadError("Download Cancelled by User!")
-
-    def add_download(self, link, path, qual):
+ 
+    def add_download(self, link, path):
         self.__onDownloadStart()
-        self.extractMetaData(link, qual)
+        self.extractMetaData(link)
         LOGGER.info(f"Downloading with YT-DL: {link}")
         self.__gid = f"{self.vid_id}{self.__listener.uid}"
-        if qual == "audio":
-          self.opts['format'] = 'bestaudio/best'
-          self.opts['postprocessors'] = [{'key': 'FFmpegExtractAudio','preferredcodec': 'mp3','preferredquality': '192',}]
-        else:
-          self.opts['format'] = qual
         if not self.is_playlist:
             self.opts['outtmpl'] = f"{path}/{self.name}"
         else:
             self.opts['outtmpl'] = f"{path}/{self.name}/%(title)s.%(ext)s"
         self.__download(link)
-
+ 
     def cancel_download(self):
         self.is_cancelled = True
