727b789b03111acf3b81c78734f380dba0890674
ADIB AKHTAB <66148227+MaxxRider@users.noreply.github.com>
2021-01-06 14:50:51 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -5,14 +5,14 @@ from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.ext_utils.bot_utils import new_thread
 from bot import dispatcher
-
-
+ 
+ 
 @new_thread
 def cloneNode(update,context):
     args = update.message.text.split(" ",maxsplit=1)
     if len(args) > 1:
         link = args[1]
-        msg = sendMessage(f"Cloning: <code>{link}</code>",context.bot,update)
+        msg = sendMessage(f"𝗖𝗹𝗼𝗻𝗶𝗻𝗴..𝗪𝗮𝗶𝘁 𝗽𝗹𝘀.🤓\n\n Link: <code>{link}</code>",context.bot,update)
         gd = GoogleDriveHelper()
         result, button = gd.clone(link)
         deleteMessage(context.bot,msg)
@@ -21,7 +21,7 @@ def cloneNode(update,context):
         else:
             sendMarkup(result,context.bot,update,button)
     else:
-        sendMessage("Provide G-Drive Shareable Link to Clone.",context.bot,update)
-
+        sendMessage("Provide G-Drive Shareable Link to Clone😐.",context.bot,update)
+ 
 clone_handler = CommandHandler(BotCommands.CloneCommand,cloneNode,filters=CustomFilters.authorized_chat | CustomFilters.authorized_user)
 dispatcher.add_handler(clone_handler)
