ce3b5bc90448defab82cb861cfc9bf7e31df51b5
ADIB AKHTAB <66148227+MaxxRider@users.noreply.github.com>
2021-01-06 14:50:10 +0530

    Update cancel_mirror.py

--- bot/modules/cancel_mirror.py
+++ bot/modules/cancel_mirror.py
@@ -1,15 +1,15 @@
 from telegram.ext import CommandHandler, run_async
-
+ 
 from bot import download_dict, dispatcher, download_dict_lock, DOWNLOAD_DIR
 from bot.helper.ext_utils.fs_utils import clean_download
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.message_utils import *
-
+ 
 from time import sleep
 from bot.helper.ext_utils.bot_utils import getDownloadByGid, MirrorStatus
-
-
+ 
+ 
 @run_async
 def cancel_mirror(update, context):
     args = update.message.text.split(" ", maxsplit=1)
@@ -49,8 +49,8 @@ def cancel_mirror(update, context):
         dl.download().cancel_download()
     sleep(1)  # Wait a Second For Aria2 To free Resources.
     clean_download(f'{DOWNLOAD_DIR}{mirror_message.message_id}/')
-
-
+ 
+ 
 @run_async
 def cancel_all(update, context):
     with download_dict_lock:
@@ -62,8 +62,8 @@ def cancel_all(update, context):
                 count += 1
     delete_all_messages()
     sendMessage(f'Cancelled {count} downloads!', context.bot, update)
-
-
+ 
+ 
 cancel_mirror_handler = CommandHandler(BotCommands.CancelMirror, cancel_mirror,
                                        filters=(CustomFilters.authorized_chat | CustomFilters.authorized_user) & CustomFilters.mirror_owner_filter)
 cancel_all_handler = CommandHandler(BotCommands.CancelAllCommand, cancel_all,
