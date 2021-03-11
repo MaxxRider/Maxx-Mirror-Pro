a5a3f9980e6ee12ea3999f2a55b77615aa0cdc72
Weeb <72728580+WeebTime@users.noreply.github.com>
2021-03-11 16:28:14 +0530

    Update mega_downloader.py

--- bot/helper/mirror_utils/download_utils/mega_downloader.py
+++ bot/helper/mirror_utils/download_utils/mega_downloader.py
@@ -7,15 +7,15 @@ from bot.helper.ext_utils.bot_utils import new_thread, get_mega_link_type
 from bot.helper.mirror_utils.status_utils.mega_download_status import MegaDownloadStatus
 import random
 import string
- 
+
 class MegaDownloaderException(Exception):
     pass
- 
- 
+
+
 class MegaAppListener(MegaListener):
     _NO_EVENT_ON = (MegaRequest.TYPE_LOGIN,MegaRequest.TYPE_FETCH_NODES)
     NO_ERROR = "no error"
- 
+
     def __init__(self, continue_event: threading.Event, listener):
         self.continue_event = continue_event
         self.node = None
@@ -30,38 +30,40 @@ class MegaAppListener(MegaListener):
         self.error = None
         self.gid = ""
         super(MegaAppListener, self).__init__()
- 
+
     @property
     def speed(self):
         """Returns speed of the download in bytes/second"""
         return self.__speed
- 
+
     @property
     def name(self):
         """Returns name of the download"""
         return self.__name
- 
+
     def setValues(self, name, size, gid):
         self.__name = name
         self.__size = size
         self.gid = gid
- 
+
     @property
     def size(self):
         """Size of download in bytes"""
         return self.__size
- 
+
     @property
     def downloaded_bytes(self):
         return self.__bytes_transferred
- 
+
     def onRequestStart(self, api, request):
         LOGGER.info('Request start ({})'.format(request))
- 
+
     def onRequestFinish(self, api, request, error):
         LOGGER.info('Mega Request finished ({}); Result: {}'
                     .format(request, error))
- 
+        if str(error).lower() != "no error":
+            self.error = error.copy()
+            return
         request_type = request.getType()
         if request_type == MegaRequest.TYPE_LOGIN:
             api.fetchNodes()
@@ -73,7 +75,7 @@ class MegaAppListener(MegaListener):
             LOGGER.info(f"Node Name: {self.node.getName()}")
         if request_type not in self._NO_EVENT_ON or self.node and "cloud drive" not in self.node.getName().lower():
             self.continue_event.set()
- 
+
     def onRequestTemporaryError(self, api, request, error: MegaError):
         LOGGER.info(f'Mega Request error in {error}')
         if not self.is_cancelled:
@@ -81,16 +83,16 @@ class MegaAppListener(MegaListener):
             self.is_cancelled = True
         self.error = error.toString()
         self.continue_event.set()
- 
+
     def onTransferStart(self, api: MegaApi, transfer: MegaTransfer):
         LOGGER.info(f"Transfer Started: {transfer.getFileName()}")
- 
+
     def onTransferUpdate(self, api: MegaApi, transfer: MegaTransfer):
         if self.is_cancelled:
             api.cancelTransfer(transfer, None)
         self.__speed = transfer.getSpeed()
         self.__bytes_transferred = transfer.getTransferredBytes()
- 
+
     def onTransferFinish(self, api: MegaApi, transfer: MegaTransfer, error):
         try:
             LOGGER.info(f'Transfer finished ({transfer}); Result: {transfer.getFileName()}')
@@ -99,34 +101,44 @@ class MegaAppListener(MegaListener):
                 self.continue_event.set()
         except Exception as e:
             LOGGER.error(e)
- 
+
     def onTransferTemporaryError(self, api, transfer, error):
-        LOGGER.info(f'Mega download error in file {transfer} {transfer.getFileName()}: {error}')
-        self.error = error.toString()
+        filen = transfer.getFileName()
+        state = transfer.getState()
+        errStr = error.toString()
+        LOGGER.info(f'Mega download error in file {transfer} {filen}: {error}')
+
+        if state == 1 or state == 4:
+            # Sometimes MEGA (offical client) can't stream a node either and raises a temp failed error.
+            # Don't break the transfer queue if transfer's in queued (1) or retrying (4) state [causes seg fault]
+            return
+
+        self.error = errStr
         if not self.is_cancelled:
             self.is_cancelled = True
-            self.listener.onDownloadError("TransferTempError: "+self.error)
- 
+            self.listener.onDownloadError(f"TransferTempError: {errStr} ({filen})")
+
     def cancel_download(self):
         self.is_cancelled = True
         self.listener.onDownloadError("Download Canceled by user")
- 
- 
+
+
 class AsyncExecutor:
- 
+
     def __init__(self):
         self.continue_event = threading.Event()
- 
+
     def do(self, function, args):
         self.continue_event.clear()
         function(*args)
         self.continue_event.wait()
- 
- 
+
+listeners = []
+
 class MegaDownloadHelper:
     def __init__(self):
         pass
- 
+
     @staticmethod
     @new_thread
     def add_download(mega_link: str, path: str, listener):
@@ -134,7 +146,9 @@ class MegaDownloadHelper:
             raise MegaDownloaderException('Mega API KEY not provided! Cannot mirror mega links')
         executor = AsyncExecutor()
         api = MegaApi(MEGA_API_KEY, None, None, 'telegram-mirror-bot')
+        global listeners
         mega_listener = MegaAppListener(executor.continue_event, listener)
+        listeners.append(mega_listener)
         with download_dict_lock:
             download_dict[listener.uid] = MegaDownloadStatus(mega_listener, listener)
         os.makedirs(path)
